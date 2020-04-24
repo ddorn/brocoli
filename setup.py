62046b519ce0079fcad8c2ecefb1f47b3df3c659
ddorn <diego.dorn@free.fr>
2020-04-24 18:59:39 +0200

    :bookmark: add setup.py back since I apparently can't build this on nix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except FileNotFoundError:
 
 setup(
     name="brocoli",
-    version="0.0.1",
+    version="0.3.2",
     packages=["brocoli"],
     url="https://github.com/ddorn/brocoli",
     license="MIT",
